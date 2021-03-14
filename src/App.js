cca0873dbf815d42b18f7fe4e2e9c6a155c1d3c1
Kavya Shree <kavyashree@macusers-MacBook-Pro-4.local>
2021-03-14 23:34:09 +0530

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import Leaderboard from "./pages/Leaderboard/Leaderboard";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import NewsPage from "./pages/NewsPage/NewsPage";
 
 function App() {
   return (
     <div className="App">
-     <p>Welcome to Stock Market Simulation 2021</p>
+      <p>Welcome to Stock Market Simulation 2021</p>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login"  component={LoginPage} />
+          <Route path="/leaderboard"  component={Leaderboard} />
+          <Route path="/dashboard"  component={Dashboard} />
+          <Route path="/news"  component={NewsPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
