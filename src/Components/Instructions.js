f451b945cfa18a3081e8b336727f77e6f5cd0fd5
Pratham Gupta <54350608+pratham1002@users.noreply.github.com>
2021-03-19 14:56:48 +0000

    Added instructions, navbar, changed login

--- src/Components/Instructions.js
+++ src/Components/Instructions.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import React from "react";
 import styles from "./Instructions.module.css";
+import { Modal } from "shards-react";
 
 class Instructions extends Component {
   constructor(props) {
@@ -15,10 +16,12 @@ class Instructions extends Component {
   }
 
   render() {
-    const classes = this.state.visible ? styles.box_vis : styles.box_hid;
-
     return (
-      <div className={classes}>
+      <Modal
+        className={styles.box}
+        open={this.props.modalVisible}
+        toggle={this.props.toggleModal}
+      >
         <p>
           <b>Instructions</b>
         </p>
@@ -29,12 +32,19 @@ class Instructions extends Component {
           1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non morbi
           semper commodo nisl condimentum lectus. Neque dolor euismod rhoncus
           et. <br />
+          1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 1. Lorem
+          ipsum dolor sit amet, consectetur adipiscing elit. Non morbi semper
+          commodo nisl condimentum lectus. Neque dolor euismod rhoncus et.{" "}
+          <br />
+          1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non morbi
+          semper commodo nisl condimentum lectus. Neque dolor euismod rhoncus
+          et. <br />
           1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
-        <p onClick={this.changeVisibilty.bind(this)}>
+        <p onClick={this.props.toggleModal}>
           <b>BACK</b>
         </p>
-      </div>
+      </Modal>
     );
   }
 }
